47d842bc82258d545d8587c310d52a35311041a8
murayzes <murayzes@gmail.com>
2019-07-19 16:32:04 +0300

    cleaned the code

--- script.js
+++ script.js
@@ -1,91 +1,44 @@
-// let numMonths, numWeeks, numDays, numHours, numMinutes, numSecond, result;
-// let countDate = new Date("jan 1, 2020 15:37:25").getTime();
-let localStorageValue;
-let datePickerValue = localStorage.getItem(localStorageValue); 
+const $bodyTitleToggle = document.querySelector(".body__title-toggle");
+const $datePicker = document.querySelector(".date-picker");
+const $counter = document.querySelector(".counter");
 
-const $counter = document.querySelector('.counter');
-const $datePicker = document.querySelector('.date-picker');
-const $dataParagraphToggle = document.querySelector('.data__pragraph-toggle');
-const $bodyTitleToggle = document.querySelector('.body__title-toggle');
+let datePickerValue = localStorage.getItem("localStorageDate");
 
-// const $years = document.querySelector('.years');
-// const $months = document.querySelector('.months');
-// const $weeks = document.querySelector('.weeks');
-// const $days = document.querySelector('.days');
-// const $hours = document.querySelector('.hours')
-// const $minutes = document.querySelector('.minutes')
-// const $seconds = document.querySelector('.seconds')
+$datePicker.setAttribute("value", datePickerValue);
 
-$datePicker.setAttribute('value', datePickerValue);
-
-$datePicker.addEventListener('change', function() {
+$datePicker.addEventListener("change", function() {
   datePickerValue = $datePicker.value;
-  localStorage.setItem(localStorageValue, datePickerValue);
+  localStorage.setItem("localStorageDate", datePickerValue);
 });
 
 setInterval(() => {
-  // value =  dateCalculate(countDate, new Date());
+  let counter = moment().countdown(
+    datePickerValue,
+      countdown.YEARS|
+      countdown.MONTHS|
+      countdown.WEEKS|
+      countdown.DAYS|
+      countdown.HOURS|
+      countdown.MINUTES|
+      countdown.SECONDS
+  );
 
-  // setData($years, getYears(value));
-  // setData($months, getMonths(value));
-  // setData($weeks, getWeeks(value));
-  // setData($days, getDays(value));
-  // setData($hours, getHours(value));
-  // setData($minutes, getMinutes(value));
-  // setData($seconds, getSeconds(value));
-  let counter = moment().countdown(datePickerValue, countdown.YEARS|countdown.MONTHS|
-    countdown.WEEKS|countdown.DAYS|countdown.HOURS|countdown.MINUTES|countdown.SECONDS);
+  if (counter.value == 0) getMessage("The time has come!!!");
+  $bodyTitleToggle.innerHTML =
+    counter.value > 0
+      ? setBodyTitleToggle("By this time, its left:")
+      : setBodyTitleToggle("After that moment it took:");
+
+  if (isNaN(counter.value)) setBodyTitleToggle("Pleace,  input full date!!!");
 
   setData($counter, counter);
-
-  if(counter.value == 0) getMessage();
-  $bodyTitleToggle.innerHTML = counter.value > 0
-    ? 'By this time, its left:' 
-    : 'After that moment it took:';
-  if (isNaN(counter.value)) $bodyTitleToggle.innerHTML = 'Pleace,  input full date!!!';
-
-  // if (result <= 0) alert('YoHooooo!!!');
-  // if (seconds < 10) seconds = '0' + seconds;
-  // if (minutes < 10) minutes = '0' + minutes;
-  // if (hours < 10) hours = '0' + hours;
 }, 1000);
 
-function getMessage() {
-  alert('The time has come!!!');
-}
+const setBodyTitleToggle = toggleStatus => ($bodyTitleToggle.innerHTML = toggleStatus);
 
-// function dateCalculate(countDate, nowDate) {
-//   return countDate - nowDate;  
-// }
+function getMessage(message) { alert(message) };
+
 
 function setData($node, timeValue) {
   $node.innerHTML = timeValue;
 }
-
-// function getSeconds(value) {
-//   return Math.floor(value % (1000 * 60) / 1000);
-// }
-
-// function getMinutes(value) {
-//   return Math.floor((value % (1000 * 60 * 60)) / (1000 * 60));
-// }
-
-// function getHours(value) {
-//   return Math.floor((value % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-// }
-
-// function getDays(value) {
-//   return Math.floor((value / (1000 * 60 * 60 * 24) % 24)% 7);
-// }
-
-// function getWeeks(value) {
-//   return Math.floor((value / (1000 * 60 * 60 * 24) % 7) / 4);
-// }
-
-// function getMonths(value) {
-//   return Math.floor(((value / (1000 * 60 * 60 * 24) % 7) / 4) / 4.1);
-// }
-
-// function getYears(value) {
-//   return Math.floor((value / (1000 * 60 * 60 * 24)) / 365) - 1;
-// }
